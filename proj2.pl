48279589b11cee449edfe2752e53941a72cf44c7
Will_Zhu <will_zyq@qq.com>
2020-05-26 00:23:03 +1000

    get all slots

--- proj2.pl
+++ proj2.pl
@@ -1,16 +1,27 @@
 % load clpfd library
 :- ensure_loaded(library(clpfd)).
 
-% puzzle_solution(Puzzle, WorldList) :-
-%     maplist(same_length(Puzzle), Puzzle),
-%     Rows = Puzzle,
-%     transpose(Puzzle, Cols)
+get_puzzle_slots(Puzzle, WorldList) :-
+    % maplist(same_length(Puzzle), Puzzle),
+    Rows = Puzzle,
+    get_all_slots(Rows, AllRowsSlots),
+    transpose(Puzzle, Cols),
+    get_all_slots(Cols, AllColsSlots),
+    append(AllRowsSlots, AllColsSlots, WorldList).
+
+
+get_all_slots([], []).
+get_all_slots([Row|Rows], AllSlots) :-
+    get_slots(Row, Slots),
+    get_all_slots(Rows, AllSlots1),
+    append(Slots, AllSlots1, AllSlots).
 
 
 get_slots(Row, Slots) :-
     get_slots(Row, [[]], Slots).
 
-get_slots([], Slots0, Slots0).
+get_slots([], Slots0, Slots) :-
+    delete(Slots0, [], Slots).
 
 get_slots(['#'|Rs], Slots0, Slots) :-
     last(Slots0, LastSlot), 
@@ -22,7 +33,7 @@ get_slots(['#'|Rs], Slots0, Slots) :-
 
 
 get_slots([A|Rs], Slots0, Slots) :-
-    % var(A),
+    A \== '#',
     length(Slots0, N),
     nth1(N, Slots0, LastSlot, RestSlots),
     append(LastSlot, [A], NewSlot),
